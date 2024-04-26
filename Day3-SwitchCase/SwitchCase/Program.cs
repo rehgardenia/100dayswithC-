be2618dcf698235df9a70d6c09b5450351438e9b
Renata Gardenia <r.gardenia06@gmail.com>
2024-04-26 19:43:30 -0300

    Dia 3

---
+++ Day3-SwitchCase/SwitchCase/Program.cs
@@ -0,0 +1,52 @@
+﻿//switch-case
+
+string sku = "01-MN-L";
+string[] produto = sku.Split('-');
+
+string tipo ="";
+string cor = "";
+string tamanho ="";
+
+switch (produto[0])
+{
+    case "01":
+        tipo ="Swet Shirt";
+        break;
+    case "02":
+        tipo ="T-Shirt";
+        break;
+    case "03":
+        tipo ="Sweat pants";
+        break;
+    default:
+        tipo ="Other";
+        break;
+}
+switch (produto[1])
+{
+    case "BL":
+        cor ="Black";
+        break;
+    case "MN":
+        cor ="Maroom";
+        break;
+    default:
+        cor ="White";
+        break;
+}
+switch (produto[2])
+{
+    case "S":
+        tamanho ="Small";
+        break;
+    case "M":
+        tamanho ="Medium";
+        break;
+    case "L":
+        tamanho ="Large";
+        break;
+    default:
+        tamanho ="One size fits all";
+        break;
+}
+Console.WriteLine($"Produto: {tipo} {cor} {tamanho}");
---
+++ Day3-SwitchCase/logicaDecisao/Program.cs
@@ -0,0 +1,29 @@
+﻿
+// Desafio da lógica de decisão
+
+string permissão = "Admin|Manager";
+int nivel = 55;
+
+
+if (permissão.Contains("Admin"))
+{
+    if (nivel > 55)
+        Console.WriteLine("Bem Vindo, Super Usuário Administrador!");
+    else
+        Console.WriteLine("Bem Vindo, Usuário Administrador!");
+}
+else if (permissão.Contains("Manager"))
+{
+    if (nivel >= 20)
+    {
+        Console.WriteLine("Contate um administrador para acessar seu usuário.");
+    }
+    else
+    {
+        Console.WriteLine("Você não tem privilégios suficiente para o acesso.");
+    }
+}
+else
+{
+    Console.WriteLine("Você não tem privilégios suficiente para o acesso.");
+}
