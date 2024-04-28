8603f24d7b36ee66fdb437738dd7dfd9357b6062
Renata Gardenia <r.gardenia06@gmail.com>
2024-04-28 19:59:45 -0300

    Dia 4 - códigos

---
+++ Day4-ForWhile/BatalhaRPG/Program.cs
@@ -0,0 +1,21 @@
+﻿int vidaHeroi = 10;
+int vidaMostro = 10;
+Random rd = new Random();
+
+do
+{
+    int ataque = rd.Next(1, 11);
+
+    vidaMostro -= ataque;
+    Console.WriteLine($"O mostro foi atacado dano de {ataque}, vida restante: {vidaMostro}");
+   
+    if (vidaMostro <= 0) continue;
+
+    ataque = rd.Next(1, 11);
+    vidaHeroi -= ataque;
+    Console.WriteLine($"O Heroi foi atacado dano {ataque}, vida restante {vidaHeroi}");
+
+}
+while (vidaHeroi > 0 && vidaMostro > 0);
+
+Console.WriteLine(vidaHeroi>vidaMostro? "O Herói Venceu!": "O Mostro Venceu!");
---
+++ Day4-ForWhile/FizzBuzz/Program.cs
@@ -0,0 +1,19 @@
+﻿//percorrer dp 1 a 100
+for (int i = 1; i <= 100; i++)
+{
+    if ((i % 3 == 0) && (i % 5 == 0))
+    {
+        Console.WriteLine(i + " - FizzBuzz");
+    }
+    else if (i % 3 == 0)
+    {
+        Console.WriteLine(i + " - Fizz");
+    }
+    else if (i % 5 == 0)
+    {
+        Console.WriteLine(i + " - Buzz");
+    }
+    else{
+        Console.WriteLine(i);
+    }
+}
