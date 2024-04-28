fdac0e97505d6312973262bb43575ca8d8eb2852
Renata Gardenia <r.gardenia06@gmail.com>
2024-04-28 20:20:13 -0300

    Dia 4 - Validar Número

---
+++ Day4-ForWhile/ValidarNumero/Program.cs
@@ -0,0 +1,24 @@
+﻿
+string? entrada;
+bool validarEntrada = false;
+bool validarNumero = false;
+int valor = 0;
+Console.WriteLine("Informe um valor inteiro entre 5 e 10: ");
+while(validarEntrada == false){
+    entrada = Console.ReadLine();
+    if(entrada != null){
+        validarNumero = int.TryParse(entrada, out valor);
+        if(validarNumero){
+            if((valor >= 5) && (valor <= 10)){
+                Console.WriteLine($"{valor} - Valor de entrada válido.");
+                validarEntrada = true;
+            }
+            else{
+                Console.WriteLine("Informe um valor dentro do intervalo solicitado.");
+            }
+        }
+        else{
+            Console.WriteLine("Informe um valor válido!");
+        }
+    }
+}
